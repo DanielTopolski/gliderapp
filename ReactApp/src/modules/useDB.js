286ecff1cc29f886a2e21d32b6f448969e3c18ac
Michał Drobiazg <dromit83@gmail.com>
2025-03-21 13:44:40 +0100

    Update useDB.js

--- ReactApp/src/modules/useDB.js
+++ ReactApp/src/modules/useDB.js
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react"
+import { createClient } from "@supabase/supabase-js";
 
+
+const project_url = "https://droqvfqshrxaorewhurw.supabase.co";
+const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyb3F2ZnFzaHJ4YW9yZXdodXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNDc4MjYsImV4cCI6MjA1NzgyMzgyNn0.c8a_gtJGeCwP78UYegLl6HnhlVgWk3E21PgVnOxlNmo";
+const supabase = createClient(project_url, anon_key);
 
 export default function useDB(tableName, properties) {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:3000/' + tableName + '.json')
-          .then((res) => res.json())
-          .then((data) => setData(data));
-        }, [tableName]);
-    
+      getDataFromDB();
+    }, );
+
+    async function getDataFromDB() {
+      const { data } = await supabase.from(tableName).select();
+      setData(data);
+    }
+
     return data;
 }
