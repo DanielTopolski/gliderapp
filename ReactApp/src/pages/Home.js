2172c35e93b19b2b0f7f491966a7838afc20943e
Michalek83 <dromit83@gmail.com>
2025-03-26 09:09:21 +0100

    Update Home.js

--- ReactApp/src/pages/Home.js
+++ ReactApp/src/pages/Home.js
@@ -3,7 +3,6 @@ export default function Home() {
         <>
             <h1>This is the main page.</h1>
             <h2>Please choose one of the above to view requested data.</h2>
-            <h3>There will some useful stuff here, I can almost guarantee it. If not...well, it'll be deleted.</h3>
         </>
     );
 }
