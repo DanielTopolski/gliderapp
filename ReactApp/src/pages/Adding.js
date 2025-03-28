ea97b6f56bd79bb1ae3847addba86a3d98200271
Michał Drobiazg <dromit83@gmail.com>
2025-03-28 15:21:59 +0100

    Update Adding.js

--- ReactApp/src/pages/Adding.js
+++ ReactApp/src/pages/Adding.js
@@ -5,6 +5,7 @@ import styles from "../App.module.css";
 //import { Column } from 'primereact/column';
 //import { useTranslation } from 'react-i18next';
 import { Dropdown } from 'primereact/dropdown';
+import { Calendar } from 'primereact/calendar';
 //import { FloatLabel } from "primereact/floatlabel";
 
 
@@ -16,6 +17,7 @@ const FlightAdding = () => {
     const [selectedPilot, setSelectedPilot] = useState(null);
     const [selectedInstr, setSelectedInstr] = useState(null);
     const [selectedAircraft, setSelectedAircraft] = useState(null);
+    const [datetime24h, setDateTime24h] = useState(null);
 
     return (
         <div>
@@ -30,6 +32,12 @@ const FlightAdding = () => {
             <div className="card flex justify-content-center">
                     <h4 className={styles.pageHeader}>Aircraft</h4>
                     <Dropdown inputId="pname" value={selectedAircraft} placeholder="Select an aircraft" onChange={(e) => setSelectedAircraft(e.value)} options={aircr} optionLabel="aircraft_data" className="w-full" />
+            </div>
+            <div className="card flex flex-wrap gap-3 p-fluid">
+            <div className="flex-auto">
+                <h4 className={styles.pageHeader}>Flight start time</h4>
+                <Calendar id="calendar-24h" value={datetime24h} onChange={(e) => setDateTime24h(e.value)} showButtonBar showTime hourFormat="24" />
+            </div>
             </div>
         </div>
     );
